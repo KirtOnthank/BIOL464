c081fb2659a1285bcf4fca9bbf60b2b34fd858c5
Kirt Onthank <Kirt.Onthank@gmail.com>
2019-03-13 15:58:51 -0700

    updated findbpm() function to be able to search in select bpm bounds

--- R/findbpm.R
+++ R/findbpm.R
@@ -6,10 +6,14 @@
 #' @return a single integer, the average heart rate in bpm, will be returned
 #' @export
 
-findbpm=function(x,t=30){
+findbpm=function(x,t=30,bounds=c(0,10000)){
   temp1=length(x)*9
   x=x-mean(x)
   padded=c(x, seq(0, 0, length=temp1))
-  bpm=(which.max(spectrum(padded)$spec)/10)*(60/t)
+  little=(bounds/(60/t))*10
+  spec=spectrum(padded)$spec
+  spec.little=spec[little[1]:little[2]]
+  bpm=((which.max(spec.little)+little[1])/10)*(60/t)
   return(paste(bpm, "Beats Per Minute"))
 }
+
